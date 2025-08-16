80bbf8a211152ee3ef24cbc48a733bda233228e7
Abishasaminathan <164978184+Abishasaminathan@users.noreply.github.com>
2025-08-16 14:29:35 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import "./Contact.css";
+import "./contact.css";
 
 export default function Contact() {
   const onSubmit = (e) => {
